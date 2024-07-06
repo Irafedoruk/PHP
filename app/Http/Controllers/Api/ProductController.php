853ac24217bde4d89fab2eb9c71dade81bec6e9c
Irafedoruk <irrnika@gmail.com>
2024-07-06 17:22:19 +0300

    change product list

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,44 +3,68 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Product;
-use Illuminate\Http\Request;
 use App\Models\ProductImage;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager;
+
 class ProductController extends Controller
 {
+    protected string $upload;
+    protected array $sizes = [50, 150, 300, 600, 1200];
+
+    public function __construct()
+    {
+        $this->upload = env("UPLOAD_DIR");
+    }
+
     public function getAll(Request $request): \Illuminate\Http\JsonResponse
     {
-        $id=$request->query("categoryId");
-        $items = Product::with(["category","product_images"])
-            ->where("category_id", "=",$id)->get();
-        return response()->json($items) ->header('Content-Type', 'application/json; charset=utf-8');
+        $id = $request->query("categoryId");
+        $items = Product::with(["category", "product_images"])
+            ->where("category_id", "=", $id)->get();
+        return response()->json($items)->header('Content-Type', 'application/json; charset=utf-8');
     }
 
     public function store(Request $request)
     {
-        $product = Product::create($request->only(['name', 'description', 'price', 'quantity', 'category_id']));
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'price' => 'required|numeric',
+            'quantity' => 'required|integer',
+            'category_id' => 'required|exists:categories,id'
+        ]);
+        $product = Product::create($validatedData);
 
-        if($request->hasFile('images')) {
-            foreach ($request->file('images') as $file) {
-                $path = $file->store('public/products');
+        if ($request->hasFile('images')) {
+            $images = $request->file("images");
+            $i = 0;
+            foreach ($images as $file) {
+                $fileName = $this->saveImage($file);
                 ProductImage::create([
-                    'name' => basename($path),
+                    'name' => $fileName,
+                    'priority' => $i++, // You can adjust this as needed
                     'product_id' => $product->id,
-                    'priority' => 0 // Можливо, вам знадобиться логіка для визначення пріоритету
                 ]);
             }
         }
-
         return response()->json($product, 201);
     }
 
-    public function destroy($id) {
-        $product = Product::find($id);
-        if($product) {
-            $product->delete();
-            return response()->json(['message' => 'Product deleted successfully'], 200);
+    protected function saveImage(UploadedFile $file)
+    {
+        $fileName = uniqid() . ".webp";
+        $manager = new ImageManager(new Driver());
+        foreach ($this->sizes as $size) {
+            $imageSave = $manager->read($file);
+            $imageSave->scale(width: $size);
+            $path = public_path($this->upload . $size . "_" . $fileName);
+            $imageSave->toWebp()->save($path);
         }
-        return response()->json(['message' => 'Product not found'], 404);
+        return $fileName;
     }
 }
